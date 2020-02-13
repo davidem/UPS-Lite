a438756528c26d2c93bdc67886b95fe12317aef3
davidem <davidem@DaBookPro.local>
2020-02-13 09:33:01 +0100

    yapf

--- UPS_Lite.py
+++ UPS_Lite.py
@@ -25,7 +25,7 @@ class UPS():
 
                 except FileNotFoundError:
                         prev_capacity = "1000"
-                        prev_voltage = "1000"
+                        prev_voltage = "1000.0001"
                 return float(prev_voltage),int(prev_capacity)
 
 
