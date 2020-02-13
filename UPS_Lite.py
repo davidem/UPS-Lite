b2e341365b745847c163b3fc7bd7ac457461e13b
davidem <davidem@DaBookPro.local>
2020-02-13 14:09:22 +0100

    added prev_status. voltage fluctuates too much.

--- UPS_Lite.py
+++ UPS_Lite.py
@@ -21,12 +21,12 @@ class UPS():
                     with open("/tmp/ups_lite_capacity.tmp","r") as tmpfile:
                         all_values = tmpfile.read()
 
-                        prev_voltage,prev_capacity = all_values.split(':')
+                        prev_voltage,prev_capacity,prev_status = all_values.split(':')
 
                 except FileNotFoundError:
                         prev_capacity = "1000"
                         prev_voltage = "1000.0001"
-                return float(prev_voltage),int(prev_capacity)
+                return float(prev_voltage),int(prev_capacity),prev_status
 
 
         def read_voltage(self):
@@ -39,7 +39,6 @@ class UPS():
                 # Write voltage to tempfile. Needed to determine state.
                 tmpfile= open("/tmp/ups_lite_capacity.tmp","w+")
                 tmpfile.write(str(voltage))
-                tmpfile.write(":")
                 tmpfile.close
                 return voltage
 
@@ -54,27 +53,38 @@ class UPS():
 
                 # Write capacity to tempfile. Needed to determine state.
                 tmpfile= open("/tmp/ups_lite_capacity.tmp","a+")
+                tmpfile.write(":")
                 tmpfile.write(str(capacity))
                 tmpfile.close
 
                 return capacity
 
-        def read_status(self,capacity,voltage,prev_capacity,prev_voltage):
+        def read_status(self,capacity,voltage,prev_capacity,prev_voltage,prev_status):
 
                 # This function returns the status of  the battery: # C: low,charged,up/down, V: going up/down
                 if(capacity >= self.full_capacity):
                     status = "CHARGED"
                 elif(prev_capacity == "1000"):
                     status = "Too_soon_to_tell"
-                elif(float(prev_voltage) > float(voltage)) and (int(prev_capacity) >= int(capacity)):
+                # low if not charging and below 20 based on voltage, else discharging
+                elif((int(prev_capacity) > int(capacity)):
                     if(capacity <= self.low_capacity):
                         status = "LOW"
                     else:
                         status = "DISCHARGING"
-                elif(float(prev_voltage) <= float(voltage)) and (int(prev_capacity) <= int(capacity)):
+                elif(int(prev_capacity) < int(capacity)):
+                    low check
                     status = "CHARGING"
+                elif(int(prev_capacity) == int(capacity)):
+                    status = prev_status
                 else:
                     status = "Too_soon_to_tell"
+
+                # Append status to tmp File
+                tmpfile= open("/tmp/ups_lite_capacity.tmp","a+")
+                tmpfile.write(":")
+                tmpfile.write(str(status))
+                tmpfile.close
                 return status
 
 
@@ -88,10 +98,10 @@ class UPS():
 def main():
 
         ups_lite = UPS()
-        prev_voltage,prev_capacity = ups_lite.read_prev_values()
+        prev_voltage,prev_capacity,prev_status = ups_lite.read_prev_values()
         voltage = ups_lite.read_voltage()
         capacity = ups_lite.read_capacity()
-        status = ups_lite.read_status(capacity,voltage,prev_capacity,prev_voltage)
+        status = ups_lite.read_status(capacity,voltage,prev_capacity,prev_voltage,prev_status)
         temp = ups_lite.read_temp()
 
         print ("**********************************************")
